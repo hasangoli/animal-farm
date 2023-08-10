ec2fb88a433e171f819541c55e56b11de24f7b42
Hasan <mrhasangoli@gmail.com>
2023-08-10 21:07:23 +0330

    Updated!

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,46 @@
+import { useEffect, useState } from 'react';
+
 const App = () => {
-  return <div></div>;
+  const [animals, setAnimals] = useState();
+
+  useEffect(() => {
+    const lastQuery = localStorage.getItem('lastQuery');
+    search(lastQuery);
+  }, []);
+
+  const search = async q => {
+    const response = await fetch(
+      `http://localhost:8080?${new URLSearchParams({ q })}`
+    );
+    const data = await response.json();
+    setAnimals(data);
+
+    localStorage.setItem('lastQuery', q);
+  };
+
+  return (
+    <div>
+      <h1>Animal Farm</h1>
+
+      <input
+        type='text'
+        placeholder='Search...'
+        onChange={e => search(e.target.value)}
+      />
+
+      <ul>
+        {animals?.length > 0 ? (
+          animals?.map(animal => (
+            <li key={animal?.id}>
+              <strong>{animal?.type}</strong> {animal?.name} {animal?.age}
+            </li>
+          ))
+        ) : (
+          <li>'No Animals Found!'</li>
+        )}
+      </ul>
+    </div>
+  );
 };
 
 export default App;
